20ea150ff5627cab2e7ba3584a46306619fd93cd
Shane833 <williamsshane883@gmail.com>
2024-01-21 21:34:57 +0530

    PyQt5 Tutorials

---
+++ Java - The Complete Reference/1- Basics/Basics.java
@@ -0,0 +1,12 @@
+public class Basics
+{
+	public static void main(String args[])
+	{
+		// There are various primitive datatypes defined in java such as
+		// Integer data types
+		int a;
+		
+		
+		
+	}
+}

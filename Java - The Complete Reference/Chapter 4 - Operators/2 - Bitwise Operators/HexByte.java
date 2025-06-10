333dc04d3e0b6b947195d519bbfc0d2630f7a240
Shane833 <williamsshane883@gmail.com>
2025-06-10 13:25:57 +0530

    Learned about Bitwise Operators
    Bitwise NOT, AND, OR, XOR, R.SHIFT, L.SHIFT

---
+++ Java - The Complete Reference/Chapter 4 - Operators/2 - Bitwise Operators/HexByte.java
@@ -0,0 +1,18 @@
+// Masking Sign Extension
+class HexByte{
+	public static void main(String args[]){
+		char hex[] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+		
+		byte b = (byte) 0xf1; // Equivalent of 241 or 1111 0001
+		
+		// >> and << operators work on the values and not the operands themselves
+		// Hence when we apply b>>4, it will just give us a value of -1, instead of
+		// changing the underlying value of the variable which is -15
+
+		System.out.println("b = 0x" + hex[b>>4 & 0x0f] + hex[b & 0x0f]);
+		
+		// Explanation : b>>4 gives 1111 1111 & 0000 1111 gives 15 i.e. 'f'
+		// b is 1111 0001 & 0000 1111 gives 1 i.e. '1'
+		// hence we get 0xf1
+	}
+}
